e3b497e09f55228c0d0fcf31c87303511d5d2b55
Arkaprava Gaine <codearka2002@gmail.com>
2024-12-17 15:37:16 +0530

    some changes redis

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ const gridSize = `${resolution * width}px`;
 
 Devvit.configure({
   redditAPI: true,
+  redis : true,
 });
 
 Devvit.addCustomPostType({
@@ -268,6 +269,29 @@ Devvit.addCustomPostType({
   },
 });
 
+
+
+Devvit.addMenuItem({
+  label: 'Add my post',
+  location: 'subreddit',
+  forUserType: 'moderator',
+  onPress: async (_event, context) => {
+    const { reddit, ui } = context;
+    ui.showToast("Submitting your post - upon completion you'll navigate there.");
+    const subreddit = await reddit.getCurrentSubreddit();
+    const post = await reddit.submitPost({
+      title: 'My devvit post',
+      subredditName: subreddit.name,
+      // The preview appears while the post loads
+      preview: (
+        <vstack height="100%" width="100%" alignment="middle center">
+          <text size="large">Loading ...</text>
+        </vstack>
+      ),
+    });
+    ui.navigateTo(post);
+  },
+});
 
 type SupportedGlyphs = keyof typeof Glyphs;
 
